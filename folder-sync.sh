4f473e3593d50cd3b9dafb27d158308ea0dc9436
Toni D'Angelo <tonydangelo123@gmail.com>
2017-05-31 16:49:48 +0200

    Update folder-sync.sh
    add info rsync

--- folder-sync.sh
+++ folder-sync.sh
@@ -6,4 +6,11 @@ SERVER=user@ip
 REMOTE_DIR=/home/user_name/backup_servers/app_name
 
 ssh $SERVER "mkdir -p $REMOTE_DIR/uploads"
+
+# rsync options: 
+# -h output numbers in a human-readable format
+# -v increase verbosity
+# -r recurse into directories
+# -P keep partially transferred files and show progress during transfer
+# -t preserve times
 rsync -h -v -r -P -t $DIR $SERVER:$REMOTE_DIR/uploads
